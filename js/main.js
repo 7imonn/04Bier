cd5b17bcf5764c457e5ba86413550b02e92386cb
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-01-29 15:26:58 +0100

    load content
    load contetn without refrech

--- js/main.js
+++ js/main.js
@@ -6,5 +6,18 @@ $(function() {
     $('nav ul li').click(function(){
         $('li').removeClass('active')
         $(this).addClass("active");
-    })
+    });
+    $("#main").load("../home.html");
+    $("#Home").click(function(){
+        $("#main").load("../home.html");
+    });
+    $("#Rezepte").click(function(){
+        $("#main").load("../rezepte.html");
+    });
+    $("#Kpi").click(function(){
+        $("#main").load("../kpi.html");
+    });
+    $("#About").click(function(){
+        $("#main").load("../about.html");
+    });
   });
