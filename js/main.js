7bcc439e5cc1af75e3b4c28b828ba7107326c73d
van den Nobelen Timon, GHR-DEV-NEX-R1-BE6 <Timon.vandenNobelen@swisscom.com>
2018-02-02 16:05:13 +0100

    result anhgfangä u probiert

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(function() {
         $(this).addClass("active");
     });
     
-    $("#main").load("../kpi.html");
+    $("#main").load("../rezepte.html");
     $("#Home").click(function(){
         $("#main").load("../home.html");
     });
